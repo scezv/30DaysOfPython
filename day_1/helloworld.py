dea59d76dfc7f8226fa6e86b690bf889092037d4
scezv <susanshrestha783@gmail.com>
2022-11-06 21:42:32 +0545

    day_1

---
+++ day_1/helloworld.py
@@ -0,0 +1,26 @@
+# Day 1 - 30DaysOfPython Challenge
+import math
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+# Checking data types
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+print('The euclidian distance between (2,3) and (10,8) is: ')
+'print(((10-8)^2 + (8-2)^2 )^(1/2))'
+p = [2, 8];
+q = [8, 10];
+
+print(math.dist(p,q))
