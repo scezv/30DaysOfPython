473fe914fb01d3c0eaa33eaaebaed7af3f113a31
scezv <susanshrestha783@gmail.com>
2022-12-10 00:14:08 +0545

    day_18

--- day_18/file_manager.py
+++ day_18/file_manager.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(this_file_path)
 ENTIRE_PROJECT_DIR = os.path.dirname(BASE_DIR)
 print(BASE_DIR, ENTIRE_PROJECT_DIR)
 
-email_txt = os.path.join("templates", "email.txt")
+email_txt = os.path.join(BASE_DIR, "templates", "email.txt")
 
 content = ''
 
