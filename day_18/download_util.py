a3ed251bb164a21814200f572bdaed87afa49aaa
scezv <susanshrestha783@gmail.com>
2022-12-13 21:46:38 +0545

    day_18

---
+++ day_18/download_util.py
@@ -0,0 +1,23 @@
+import os
+import requests
+import shutil
+
+def download_file(url, directory, fname = None):
+    if fname == None:
+        fname = os.path.basename(url)
+    dl_path = os.path.join(directory, fname)
+    with requests.get(url, stream=True) as r:
+        with open(dl_path, 'wb') as file_obj:
+            shutil.copyfileobj(r.raw, file_obj)
+    return dl_path
+
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    # Note the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+    return local_filename
